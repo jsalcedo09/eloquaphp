75afb87ea0ef664d10451d66ece4a6df95a162bd
Chintan Patel <chintan.patel@qdata.io>
2016-12-19 15:16:38 +0530

    [master] Add exception for unique validation.

--- Eloqua.php
+++ Eloqua.php
@@ -14,287 +14,274 @@ use League\OAuth2\Client\Token\AccessToken;
 
 class Eloqua {
 
-	protected $baseUrl = 'https://secure.p03.eloqua.com';
+    protected $baseUrl = 'https://secure.p03.eloqua.com';
 
-	/**
-	 * @var string
-	 */
-	protected $clientId;
+    /**
+     * @var string
+     */
+    protected $clientId;
 
-	/**
-	 * @var string
-	 */
-	protected $clientSecret;
+    /**
+     * @var string
+     */
+    protected $clientSecret;
 
-	/**
-	 * @var string
-	 */
-	protected $redirectUri;
+    /**
+     * @var string
+     */
+    protected $redirectUri;
 
-	/**
-	 * @var OAuth2\Provider
-	 */
-	protected $authProvider;
+    /**
+     * @var OAuth2\Provider
+     */
+    protected $authProvider;
 
-	/**
-	 * @var \GuzzleHttp\Client
-	 */
-	protected $httpClient;
+    /**
+     * @var \GuzzleHttp\Client
+     */
+    protected $httpClient;
 
-	/**
-	 * @var Token
-	 */
-	protected $token;
+    /**
+     * @var Token
+     */
+    protected $token;
 
+    /**
+     * @var array
+     */
+    protected $refresh_callback;
 
-	/**
-	 * @var array
-	 */
-	protected $refresh_callback;
+    /**
+     * @var array
+     */
+    protected $apis = array();
 
-	/**
-	 * @var array
-	 */
-	protected $apis = array();
+    public function __construct($config = array()) {
 
-	public function __construct( $config = array() ) {
+        if (isset($config['clientId'])) {
+            $this->clientId = $config['clientId'];
+        }
 
-		if ( isset( $config['clientId'] ) ) {
-			$this->clientId = $config['clientId'];
-		}
+        if (isset($config['clientSecret'])) {
+            $this->clientSecret = $config['clientSecret'];
+        }
 
-		if ( isset( $config['clientSecret'] ) ) {
-			$this->clientSecret = $config['clientSecret'];
-		}
+        if (isset($config['redirectUri'])) {
+            $this->redirectUri = $config['redirectUri'];
+        }
+    }
 
-		if ( isset( $config['redirectUri'] ) ) {
-			$this->redirectUri = $config['redirectUri'];
-		}
+    /**
+     * @return string
+     */
+    public function get_clientId() {
+        return $this->clientId;
+    }
 
-	}
+    /**
+     * @param string $clientId
+     */
+    public function set_clientId($clientId) {
+        $this->clientId = $clientId;
+    }
 
-	/**
-	 * @return string
-	 */
-	public function get_clientId() {
-		return $this->clientId;
+    /**
+     * @return string
+     */
+    public function get_clientSecret() {
+        return $this->clientSecret;
+    }
 
-	}
+    /**
+     * @param string $clientSecret
+     */
+    public function set_clientSecret($clientSecret) {
+        $this->clientSecret = $clientSecret;
+    }
 
-	/**
-	 * @param string $clientId
-	 */
-	public function set_clientId( $clientId ) {
-		$this->clientId = $clientId;
+    /**
+     * @return string
+     */
+    public function get_redirectUri() {
+        return $this->redirectUri;
+    }
 
-	}
+    /**
+     * @param string $redirectUri
+     */
+    public function set_redirectUri($redirectUri) {
+        $this->redirectUri = $redirectUri;
+    }
 
-	/**
-	 * @return string
-	 */
-	public function get_clientSecret() {
-		return $this->clientSecret;
+    /**
+     * @return OAuth2\Provider
+     */
+    public function get_authProvider() {
 
-	}
+        if (!$this->authProvider) {
+            $config = array(
+                'clientId' => $this->clientId,
+                'clientSecret' => $this->clientSecret,
+                'redirectUri' => $this->redirectUri,
+            );
+            $this->authProvider = new Provider($config);
+        }
 
-	/**
-	 * @param string $clientSecret
-	 */
-	public function set_clientSecret( $clientSecret ) {
-		$this->clientSecret = $clientSecret;
+        return $this->authProvider;
+    }
 
-	}
+    /**
+     * @return \GuzzleHttp\Client
+     */
+    public function get_httpClient() {
+        if (!$this->httpClient) {
+            $this->httpClient = new Client(array(
+                'base_uri' => $this->baseUrl,
+                'headers' => array(
+                    'Authorization' => 'Bearer ' . $this->token->getToken(),
+                ),
+                    ));
+        }
 
-	/**
-	 * @return string
-	 */
-	public function get_redirectUri() {
-		return $this->redirectUri;
+        return $this->httpClient;
+    }
 
-	}
+    /**
+     * @param Token $token
+     */
+    public function set_token(AccessToken $token) {
 
-	/**
-	 * @param string $redirectUri
-	 */
-	public function set_redirectUri( $redirectUri ) {
-		$this->redirectUri = $redirectUri;
+        $this->token = $token;
+    }
 
-	}
+    /**
+     * @return SubscriberService
+     */
+    public function subscribers() {
 
-	/**
-	 * @return OAuth2\Provider
-	 */
-	public function get_authProvider() {
+        return $this->get_api('SubscriberService');
+    }
 
-		if ( ! $this->authProvider ) {
-			$config             = array(
-				'clientId'     => $this->clientId,
-				'clientSecret' => $this->clientSecret,
-				'redirectUri'  => $this->redirectUri,
-			);
-			$this->authProvider = new Provider( $config );
-		}
+    /**
+     * @return CampaignService
+     */
+    public function campaigns() {
 
-		return $this->authProvider;
+        return $this->get_api('CampaignService');
+    }
 
-	}
+    public function get_api($class) {
+        $fq_class = '\\EloquaApi\\Service\\' . $class;
 
-	/**
-	 * @return \GuzzleHttp\Client
-	 */
-	public function get_httpClient() {
-		if ( ! $this->httpClient ) {
-			$this->httpClient = new Client( array(
-				'base_uri' => $this->baseUrl,
-				'headers'  => array(
-					'Authorization' => 'Bearer ' . $this->token->getToken(),
-				),
-			) );
-		}
+        if (!class_exists($fq_class)) {
+            throw new ServiceNotFoundException('Service: ' . $class . ' could not be found');
+        }
 
-		return $this->httpClient;
+        if (!array_key_exists($fq_class, $this->apis)) {
+            $this->apis[$fq_class] = new $fq_class($this);
+        }
 
-	}
+        return $this->apis[$fq_class];
+    }
 
-	/**
-	 * @param Token $token
-	 */
-	public function set_token( AccessToken $token ) {
+    public function request($path = '', $method = 'get', $data = array()) {
 
-		$this->token = $token;
+        if (empty($this->baseUrl)) {
+            //$this->baseUrl = $this->getBaseUrl();
+        }
 
-	}
+        if (!$this->validate_token()) {
+            throw new Exception("Invalid Token");
+        }
 
-	/**
-	 * @return SubscriberService
-	 */
-	public function subscribers() {
+        $options = array();
 
-		return $this->get_api( 'SubscriberService' );
+        switch ($method) {
+            case 'get' :
+                if (!empty($data)) {
+                    $query = array();
+                    foreach ($data as $key => $value) {
+                        $query[$key] = $value;
+                    }
+                    $options['query'] = $query;
+                }
+                break;
+            case 'post' :
+            case 'put' :
+                if (!empty($data)) {
+                    $json = array();
+                    foreach ($data as $key => $value) {
+                        $json[$key] = $value;
+                    }
+                    $options['json'] = $json;
+                }
+                break;
+        }
 
-	}
+        try {
+            /** @var \GuzzleHttp\Psr7\Response $response * */
+            $response = $this->get_httpClient()->{$method}($path, $options);
+            return json_decode($response->getBody());
+        } catch (RequestException $e) {
+            if ($e->hasResponse() && $e->getResponse()->getStatusCode() === 422) {
+                $body = $e->getResponse()->getBody()->getContents();
+                $handler = new ValidationExceptionHandler($body);
+                $handler->handle();
+            } else if ($e->hasResponse() && $e->getResponse()->getStatusCode() === 401) {
+                
+            } else if ($e->hasResponse() && $e->getResponse()->getStatusCode() === 409) {
+                return 'uniqueValidationError';
+            }
+        } catch (\Exception $e) {
+            return $e->getMessage();
+        }
 
-	/**
-	 * @return CampaignService
-	 */
-	public function campaigns() {
+        return false;
+    }
 
-		return $this->get_api( 'CampaignService' );
+    protected function getBaseUrl() {
 
-	}
+        try {
+            if ($this->token) {
+                $client = new Client(array(
+                    'base_uri' => 'https://login.eloqua.com',
+                    'headers' => array(
+                        'Authorization' => 'Bearer ' . $this->token->getToken(),
+                    ),
+                ));
+                $response = $client->get('id');
+                $json = json_decode($response->getBody());
+                return $json->urls->base;
+            }
+        } catch (RequestException $e) {
+            if ($e->hasResponse() && $e->getResponse()->getStatusCode() === 422) {
+                $body = $e->getResponse()->getBody()->getContents();
+                $handler = new ValidationExceptionHandler($body);
+                $handler->handle();
+            }
+        } catch (\Exception $e) {
+            return $e->getMessage();
+        }
+    }
 
-	public function get_api( $class ) {
-		$fq_class = '\\EloquaApi\\Service\\' . $class;
+    public function set_refresh_callback($function) {
+        $this->refresh_callback = $function;
+    }
 
-		if ( ! class_exists( $fq_class ) ) {
-			throw new ServiceNotFoundException( 'Service: ' . $class . ' could not be found' );
-		}
+    private function validate_token() {
+        $provider = $this->get_authProvider();
+        if (!empty($this->token) && $this->token->hasExpired()) {
+            $newAccessToken = $provider->getAccessToken('refresh_token', [
+                'refresh_token' => $this->token->getRefreshToken()
+            ]);
 
-		if ( ! array_key_exists( $fq_class, $this->apis ) ) {
-			$this->apis[ $fq_class ] = new $fq_class( $this );
-		}
+            $this->set_token($newAccessToken);
+            call_user_func_array($this->refresh_callback, array($newAccessToken));
+        } else if (empty($this->token)) {
+            return false;
+        }
 
-		return $this->apis[ $fq_class ];
-	}
-
-	public function request( $path = '', $method = 'get', $data = array() ) {
-
-		if(empty($this->baseUrl)){
-			//$this->baseUrl = $this->getBaseUrl();
-		}
-
-		if(!$this->validate_token()) {
-			throw new Exception("Invalid Token");
-		}
-
-		$options = array();
-
-		switch ( $method ) {
-			case 'get' :
-				if ( ! empty( $data ) ) {
-					$query = array();
-					foreach ( $data as $key => $value ) {
-						$query[ $key ] = $value;
-					}
-					$options['query'] = $query;
-				}
-				break;
-			case 'post' :
-			case 'put' :
-				if ( ! empty( $data ) ) {
-					$json = array();
-					foreach ( $data as $key => $value ) {
-						$json[ $key ] = $value;
-					}
-					$options['json'] = $json;
-				}
-				break;
-		}
-
-		try {
-			/** @var \GuzzleHttp\Psr7\Response $response **/
-			$response = $this->get_httpClient()->{$method}( $path, $options );
-			return json_decode( $response->getBody() );
-		} catch ( RequestException $e ) {
-			if ( $e->hasResponse() && $e->getResponse()->getStatusCode() === 422 ) {
-				$body = $e->getResponse()->getBody()->getContents();
-				$handler = new ValidationExceptionHandler( $body );
-				$handler->handle();
-			}else if($e->hasResponse() && $e->getResponse()->getStatusCode() === 401){}
-		} catch ( \Exception $e ) {
-			return $e->getMessage();
-		}
-
-		return false;
-
-	}
-
-	protected function getBaseUrl(){
-
-		try {
-			if($this->token) {
-				$client = new Client(array(
-					'base_uri' => 'https://login.eloqua.com',
-					'headers' => array(
-						'Authorization' => 'Bearer ' . $this->token->getToken(),
-					),
-				));
-				$response = $client->get('id');
-				$json = json_decode($response->getBody());
-				return $json->urls->base;
-			}
-		} catch ( RequestException $e ) {
-			if ( $e->hasResponse() && $e->getResponse()->getStatusCode() === 422 ) {
-				$body = $e->getResponse()->getBody()->getContents();
-				$handler = new ValidationExceptionHandler( $body );
-				$handler->handle();
-			}
-		} catch ( \Exception $e ) {
-			return $e->getMessage();
-		}
-	}
-
-	public function set_refresh_callback($function)
-	{
-		$this->refresh_callback = $function;
-	}
-
-	private function validate_token()
-	{
-		$provider = $this->get_authProvider();
-		if (!empty($this->token) && $this->token->hasExpired()) {
-			$newAccessToken = $provider->getAccessToken('refresh_token', [
-				'refresh_token' => $this->token->getRefreshToken()
-			]);
-
-			$this->set_token($newAccessToken);
-			call_user_func_array($this->refresh_callback, array($newAccessToken));
-		}else if(empty($this->token)){
-			return false;
-		}
-
-		return true;
-	}
-
+        return true;
+    }
 
 }
